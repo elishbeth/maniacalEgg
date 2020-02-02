329a7571a5f49220d591b76f3886eb72751c5ea7
Elisabeth Landgren <elisabeth.a.landgren@gmail.com>
2020-02-02 13:49:59 -0600

    some dictionaries

--- scrape-schedules.py
+++ scrape-schedules.py
@@ -13,20 +13,40 @@ firstname = course.name
 
 
 while(course != None):
+    number = 0
+
+    classDict = {}
 
     for child in course.children:
-        if (isinstance(child, bs4.element.NavigableString)):
-            a=1
-        elif(child.a):
-            print(child.a.get('href'))
-             #href = child.find(href)
-            print("******")
-            print(child.text.strip())
-            # print(child)
+        if (number == 0):
+            category = "number"
+        if (number == 1):
+            category = "name"
+        if (number == 2):
+            category = "days"
+        if (number == 3):
+            category = "time"
+        if (number == 4):
+            category = "room"
+        if (number == 5):
+            category = "instructor"
+        if (number == 6):
+            category = "available"
 
+        if (isinstance(child, bs4.element.NavigableString)):
+            pass
+        elif(child.a):
+            #print("*link", child.a.get('href'))
+            classDict["link"] = child.a.get('href')
+            #print("---what this", child.text.replace("Details", "").strip())
+            classDict["notes"] = child.text.replace("Details", "").strip()
         else:
-            print(child.text.strip())
+            #print(")))))who this",number," ",child.text.strip())
+            classDict[category] = child.text.strip()
+            number += 1
+    print(classDict)
+    print("\n\n")
     course = course.find_next("tr")
 
 
-print(firstname)
+#print(firstname)
