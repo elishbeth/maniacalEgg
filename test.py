e94a72b2fb6f3509c402e50920873f3114e84ab6
Lena Underwood <lenaunderwood22@gmail.com>
2020-02-23 14:53:54 -0600

    details new line separator not working

--- test.py
+++ test.py
@@ -12,5 +12,25 @@ page = requests.get(url,verify = False)
 #response = requests.get(url, verify=False)
 soup = bs4.BeautifulSoup(page.content,'lxml')
 
-allText = soup.get_text())
-allText
+allText = soup.get_text()
+
+L = []
+if "\n" in allText:
+    L = allText.strip().split("\n")
+    L.remove("")
+
+count = 1
+ClassDict = {}
+for l in L:
+    if l!="" and count == 1:
+        ClassDict["Description"] = l
+        count = 4
+    if "General Education" in l:
+        count = 2
+        ClassDict["General Education"] = []
+    if "Fine Arts" in l:
+        count = 3
+    if count == 2 and l!="":
+        ClassDict["General Education"].append(l)
+    if count == 3 and l!="":
+        ClassDict["Fine Arts"] = l
