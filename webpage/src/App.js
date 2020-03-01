4f421f3b346800a377bb2fc1e8b819265fa57abc
beckaberhanu <beckaberhanu2@gmail.com>
2020-03-01 13:35:51 -0600

    installed material ui library, made a few componenets and changed the app.js file

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import CheckBox from "@material-ui/core/Checkbox";
+import NavBar from "./Components/NavBar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <NavBar />
+        </div>
+        <div className = "Checkboxes">
+          <FormControlLabel
+            control={<CheckBox label="yooooo">yoooo</CheckBox>}
+            label="yooooo"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
